c389c225e98d6f73454305512e8d088d7cf5a15a
Gregory Casamento <greg.casamento@gmail.com>
2024-08-08 20:07:38 -0400

    Add all files

--- NSDateFormatter_test/main.m
+++ NSDateFormatter_test/main.m
@@ -10,7 +10,26 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSDateFormatter *df;
+        NSTimeInterval t = 650498989.396999955177;
+        NSDate *d;
+        NSString *s;
+
+        df = [NSDateFormatter new];
+        [df setLocale:[NSLocale localeWithLocaleIdentifier:@"en_US_POSIX"]];
+        [df setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"];
+        [df setTimeZone:[NSTimeZone timeZoneForSecondsFromGMT:0]];
+
+        d = [[NSDate alloc] initWithTimeIntervalSinceReferenceDate:t];
+        s = [df stringFromDate:d];
+        if ([@"2021-08-12T22:09:49.397Z" isEqualToString:s] == NO)
+        {
+            NSLog(@"botch: should get 2021-08-12T22:09:49.397Z but got %@", s);
+        }
+        else
+        {
+            NSLog(@"matched");
+        }
     }
     return 0;
 }
